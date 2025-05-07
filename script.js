7342384d2a4d85acd92ececf76c7752519ef02b1
KariMckinney <ajpetrikov@gmail.com>
2025-05-06 22:10:03 -0500

    Added css and js

---
+++ script.js
@@ -0,0 +1,21 @@
+let sonicFactElement = document.getElementById("sonic-fact");
+
+let sonicOutput = document.getElementById("sonic-output");
+
+sonicOutput.addEventListener("click, sonicOutputEventHandler");
+
+function  sonicOutputEventHandler(event){
+    event.preventDefault();
+    sonicFactElement.innerHTML = 'In the archie comics Sonics canon name is  Olgilvie Maurice Hedgehog.'
+}
+
+let splatoonFactElement = document.getElementById("splatoon-fact");
+
+let splatoonOutput = document.getElementById("splatoon-output");
+
+splatoonOutput.addEventListener("click, splatoonOutputEventHandler");
+
+function  splatoonOutputEventHandler(event){
+    event.preventDefault();
+    splatoonFactElement.innerHTML = 'There are 8 agents in total within the game.'
+}
